07a80d3af12161a935d05604cabd84aabec1d026
Leung Cheng <monewckl@hotmail.com.hk>
2024-06-22 17:16:12 +0800

    Postgres setup

---
+++ app/postgres.py
@@ -0,0 +1,33 @@
+from dataclasses import dataclass
+import os
+import psycopg
+
+
+@dataclass(frozen=True)
+class PostgresContext:
+    host: str
+    port: int
+    user: str
+    password: str
+    database: str
+
+
+def new_postgres_context_from_env() -> PostgresContext:
+    host = os.environ.get("POSTGRES_HOST", "localhost")
+    port = int(os.getenv("POSTGRES_PORT", "5432"))
+    user = os.getenv("POSTGRES_USER", "admin")
+    password = os.getenv("POSTGRES_PASSWORD", "password")
+    database = os.getenv("POSTGRES_DATABASE", "db")
+    return PostgresContext(
+        host=host, port=port, user=user, password=password, database=database
+    )
+
+
+def new_postgres_conn(postgres_context: PostgresContext):
+    return psycopg.connect(
+        host=postgres_context.host,
+        user=postgres_context.user,
+        password=postgres_context.password,
+        dbname=postgres_context.database,
+        port=postgres_context.port,
+    )
