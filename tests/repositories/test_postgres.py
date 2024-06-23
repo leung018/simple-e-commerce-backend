4db0309c49275725962eba8da4debd34cdbf6bc0
Leung Cheng <monewckl@hotmail.com.hk>
2024-06-23 21:24:37 +0800

    Update session tests

--- tests/repositories/test_postgres.py
+++ tests/repositories/test_postgres.py
@@ -4,20 +4,15 @@ from app.repositories.order import PostgresOrderRepository
 from tests.repositories.constructor import new_order
 
 
-def new_session():
-    return PostgresSession(new_postgres_context_from_env())
-
-
 def test_should_session_not_commit_work_by_default(postgres_session: PostgresSession):
     order = new_order(user_id="u1")
     with postgres_session:
         order_repository = PostgresOrderRepository()
         order_repository.add(order, postgres_session)
+        assert len(order_repository.get_by_user_id("u1", postgres_session)) == 1
 
-    other_session = new_session()
-
-    with other_session:
-        assert len(order_repository.get_by_user_id("u1", other_session)) == 0
+    with postgres_session:
+        assert len(order_repository.get_by_user_id("u1", postgres_session)) == 0
 
 
 def test_should_session_committed_changes_persist_to_other_session(
@@ -29,10 +24,8 @@ def test_should_session_committed_changes_persist_to_other_session(
         order_repository.add(order, postgres_session)
         postgres_session.commit()
 
-    other_session = new_session()
-
-    with other_session:
-        assert len(order_repository.get_by_user_id("u1", other_session)) == 1
+    with postgres_session:
+        assert len(order_repository.get_by_user_id("u1", postgres_session)) == 1
 
 
 def test_should_rollback_on_error(postgres_session: PostgresSession):
