9e4441c812c2a13df351cd1227127f735327cecf
Leung Cheng <monewckl@hotmail.com.hk>
2024-06-23 16:13:20 +0800

    Refactor userRepo

--- app/repositories/user.py
+++ app/repositories/user.py
@@ -1,55 +1,47 @@
 from abc import ABC, abstractmethod
+from typing import Generic, SupportsAbs, TypeVar
 from app.models.user import User
-from app.repositories.postgres import new_postgres_conn, new_postgres_context_from_env
+from app.repositories.postgres import PostgresSession
+from app.repositories.session import RepositorySession
 
 
-class UserRepositoryInterface(ABC):
+S = TypeVar("S", bound=SupportsAbs[RepositorySession])
+
+
+class UserRepositoryInterface(ABC, Generic[S]):
     @abstractmethod
-    def save(self, user: User):
+    def save(self, user: User, session: S):
         pass
 
     @abstractmethod
-    def get_by_id(self, user_id: str) -> User:
+    def get_by_id(self, user_id: str, session: S) -> User:
         pass
 
 
 class PostgresUserRepository(UserRepositoryInterface):
-    def __init__(self):
-        self._context = new_postgres_context_from_env()
-        with self._conn() as conn:
-            with conn.cursor() as cur:
-                cur.execute(
-                    """
-                    CREATE TABLE IF NOT EXISTS users (
-                        id VARCHAR PRIMARY KEY,
-                        balance NUMERIC
-                    );  
+    CREATE_TABLE_IF_NOT_EXISTS = """
+        CREATE TABLE IF NOT EXISTS users (
+            id VARCHAR PRIMARY KEY,
+            balance NUMERIC
+        );
+    """
+
+    DROP_TABLE = """
+        DROP TABLE users;
+    """
+
+    def save(self, user: User, session: PostgresSession):
+        with session.conn.cursor() as cur:
+            cur.execute(
                 """
-                )
-                cur.execute(  # TODO: remove this later
-                    """
-                    DELETE FROM users;
-                    """
-                )
-            conn.commit()
+                INSERT INTO users (id, balance)
+                VALUES (%s, %s)
+            """,
+                (user.id, user.balance),
+            )
 
-    def _conn(self):
-        return new_postgres_conn(self._context)
-
-    def save(self, user: User):
-        with self._conn() as conn:
-            with conn.cursor() as cur:
-                cur.execute(
-                    """
-                    INSERT INTO users (id, balance)
-                    VALUES (%s, %s)
-                """,
-                    (user.id, user.balance),
-                )
-            conn.commit()
-
-    def get_by_id(self, user_id: str) -> User:
-        with self._conn().cursor() as cur:
+    def get_by_id(self, user_id: str, session: PostgresSession) -> User:
+        with session.conn.cursor() as cur:
             cur.execute(
                 "SELECT id, balance FROM users WHERE id = %s;",
                 (user_id,),
--- tests/repositories/conftest.py
+++ tests/repositories/conftest.py
@@ -3,6 +3,7 @@ import pytest
 
 from app.repositories.postgres import PostgresSession
 from app.repositories.product import PostgresProductRepository
+from app.repositories.user import PostgresUserRepository
 
 
 @pytest.fixture
@@ -10,9 +11,11 @@ def postgres_session() -> Generator[PostgresSession, None, None]:
     session = PostgresSession()
     with session.conn.cursor() as cur:
         cur.execute(PostgresProductRepository.CREATE_TABLE_IF_NOT_EXISTS)
+        cur.execute(PostgresUserRepository.CREATE_TABLE_IF_NOT_EXISTS)
     session.commit()
     yield session
     with session:
         with session.conn.cursor() as cur:
             cur.execute(PostgresProductRepository.DROP_TABLE)
+            cur.execute(PostgresUserRepository.DROP_TABLE)
     session.commit()
