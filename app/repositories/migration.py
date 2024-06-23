d3788e84dba6c1940c77ebdce39d30175a46762f
Leung Cheng <monewckl@hotmail.com.hk>
2024-06-23 17:47:34 +0800

    Refactor the table creation

---
+++ app/repositories/migration.py
@@ -0,0 +1,28 @@
+from app.repositories.order import PostgresOrderRepository
+from app.repositories.postgres import PostgresSession
+from app.repositories.product import PostgresProductRepository
+from app.repositories.user import PostgresUserRepository
+
+
+def set_up_tables(session: PostgresSession):
+    stmts = [
+        PostgresProductRepository.CREATE_TABLE_IF_NOT_EXISTS,
+        PostgresUserRepository.CREATE_TABLE_IF_NOT_EXISTS,
+        PostgresOrderRepository.CREATE_TABLES_IF_NOT_EXISTS,
+    ]
+
+    with session.conn.cursor() as cur:
+        for stmt in stmts:
+            cur.execute(stmt)
+
+
+def drop_tables(session: PostgresSession):
+    stmts = [
+        PostgresProductRepository.DROP_TABLE,
+        PostgresUserRepository.DROP_TABLE,
+        PostgresOrderRepository.DROP_TABLES,
+    ]
+
+    with session.conn.cursor() as cur:
+        for stmt in stmts:
+            cur.execute(stmt)
