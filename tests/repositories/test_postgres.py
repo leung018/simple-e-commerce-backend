8e7894330febde5ca0da0a1e0dfca62b0b46ded8
Leung Cheng <monewckl@hotmail.com.hk>
2024-06-24 13:29:24 +0800

    Add more test case on rollback and commit

--- tests/repositories/test_postgres.py
+++ tests/repositories/test_postgres.py
@@ -42,6 +42,27 @@ def test_should_able_to_rollback_change_in_the_same_session_block(
         assert len(order_repository.get_by_user_id("u1", postgres_session)) == 0
 
 
+def test_should_not_rollback_committed_changes(
+    postgres_session: PostgresSession,
+):
+    order1 = new_order(user_id="u1", id="o1")
+    order2 = new_order(user_id="u1", id="o2")
+
+    with postgres_session:
+        order_repository = PostgresOrderRepository()
+        order_repository.add(order1, postgres_session)
+        postgres_session.commit()
+
+        order_repository.add(order2, postgres_session)
+        assert len(order_repository.get_by_user_id("u1", postgres_session)) == 2
+
+        postgres_session.rollback()
+        assert len(order_repository.get_by_user_id("u1", postgres_session)) == 1
+
+    with postgres_session:
+        assert len(order_repository.get_by_user_id("u1", postgres_session)) == 1
+
+
 def test_should_rollback_on_error(postgres_session: PostgresSession):
     class MyException(Exception):
         pass
