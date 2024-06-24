6524f3a3636cceafe5f993beed1532e32a661cdb
Leung Cheng <monewckl@hotmail.com.hk>
2024-06-24 12:11:43 +0800

    Rename to config

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,12 +2,12 @@ from typing import Generator
 import pytest
 
 from app.repositories.migration import drop_tables, set_up_tables
-from app.repositories.postgres import PostgresSession, new_postgres_context_from_env
+from app.repositories.postgres import PostgresSession, new_postgres_config_from_env
 
 
 @pytest.fixture
 def postgres_session() -> Generator[PostgresSession, None, None]:
-    session = PostgresSession(new_postgres_context_from_env())
+    session = PostgresSession(new_postgres_config_from_env())
     with session:
         set_up_tables(session)
         session.commit()
