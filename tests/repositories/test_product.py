5222cc67d1b553daf43d4ea21fd1ddc7469fa324
Leung Cheng <monewckl@hotmail.com.hk>
2024-06-24 14:13:47 +0800

    Fix save method of product repo

--- tests/repositories/test_product.py
+++ tests/repositories/test_product.py
@@ -22,3 +22,18 @@ def test_should_raise_not_found_if_product_id_not_exist(
     with postgres_session:
         with pytest.raises(EntityNotFoundError):
             product_repository.get_by_id("unknown", postgres_session)
+
+
+def test_should_save_able_to_update_product(postgres_session: PostgresSession):
+    product = new_product(name="old name", category="old category", price=9, quantity=1)
+    product_repository = PostgresProductRepository()
+    with postgres_session:
+        product_repository.save(product, postgres_session)
+
+        product.name = "new name"
+        product.category = "new category"
+        product.price = 12
+        product.quantity = 10
+        product_repository.save(product, postgres_session)
+
+        assert product_repository.get_by_id(product.id, postgres_session) == product
