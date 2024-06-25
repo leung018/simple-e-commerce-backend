fd076a5e8bf0164f25121065bb15846070d33fd9
Leung Cheng <monewckl@hotmail.com.hk>
2024-06-25 13:15:29 +0800

    Update request model

--- app/routers/orders.py
+++ app/routers/orders.py
@@ -7,6 +7,10 @@ DUMMY_USER_ID = "dummy_user_id"  # TODO: For testing in current stage, remove it
 router = APIRouter()
 
 
+class PlaceOrderRequest(BaseModel):
+    product_id_to_quantity: dict[str, int]
+
+
 class ProductModel(BaseModel):
     id: str
     name: str
@@ -19,7 +23,7 @@ class OrderModel(BaseModel):
 
 
 @router.post("/", status_code=201)
-def place_order():
+def place_order(place_order_request: PlaceOrderRequest):
     pass
 
 
