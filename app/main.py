93607819717b8ed4803296cc6cf78c044255640c
Leung Cheng <monewckl@hotmail.com.hk>
2024-06-25 13:15:40 +0800

    Draft the test

--- app/main.py
+++ app/main.py
@@ -4,8 +4,3 @@ from app.routers.orders import router as order_router
 app = FastAPI()
 
 app.include_router(order_router, prefix="/orders", tags=["orders"])
-
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,11 +1,28 @@
 from fastapi.testclient import TestClient
 
+from app.dependencies import get_product_repository, get_user_repository
 from app.main import app
+from app.repositories.session import RepositorySession
+from app.routers.orders import DUMMY_USER_ID
+from tests.models.constructor import new_product, new_user
 
 client = TestClient(app)
 
 
-def test_read_main():
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"Hello": "World"}
+def test_should_create_and_get_order(repository_session: RepositorySession):
+    user_repository = get_user_repository()
+    product_repository = get_product_repository()
+
+    product = new_product(quantity=10, price=1)
+
+    with repository_session:
+        user_repository.save(
+            new_user(id=DUMMY_USER_ID, balance=100), repository_session
+        )
+        product_repository.save(product, repository_session)
+        repository_session.commit()
+
+    response = client.post("/orders", json={"product_id_to_quantity": {product.id: 5}})
+    assert response.status_code == 201
+
+    # TODO
