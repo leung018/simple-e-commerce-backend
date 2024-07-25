f8d1949f41eff128155337cc3da4751b3a2be3fd
Leung Cheng <monewckl@hotmail.com.hk>
2024-07-25 20:41:03 +0800

    Add for share for product query

--- tests/repositories/test_product.py
+++ tests/repositories/test_product.py
@@ -15,6 +15,10 @@ def test_should_save_and_get_product(repository_session: PostgresSession):
         assert product == product_repository.get_by_id(
             product.id,
         )
+        assert product == product_repository.get_by_id(
+            product.id,
+            explicit_lock=True,
+        )
 
 
 def test_should_raise_not_found_if_product_id_not_exist(
