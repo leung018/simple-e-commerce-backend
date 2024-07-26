a819f383137cfe772a9fbf7fe5ffbe54005d57d9
Leung Cheng <monewckl@hotmail.com.hk>
2024-07-26 15:42:39 +0800

    Refactor select_query_helper

--- app/repositories/postgres/helper.py
+++ app/repositories/postgres/helper.py
@@ -3,7 +3,12 @@ from app.repositories.base import LockLevel
 
 def select_query_helper(query: str, lock_level: LockLevel):
     query = query.strip(";")
-    if lock_level == lock_level.EXCLUSIVE:
-        query += " FOR UPDATE"
+
+    match lock_level:
+        case LockLevel.EXCLUSIVE:
+            query += " FOR UPDATE"
+        case LockLevel.NONE:
+            """Do nothing"""
+
     query += ";"
     return query
