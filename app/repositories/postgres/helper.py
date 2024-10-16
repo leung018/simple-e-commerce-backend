0e7f2ae3e32a605317acd5d1049010c976883d58
Leung Cheng <monewckl@hotmail.com.hk>
2024-10-16 11:54:10 +0800

    Rename and update comments of lock level

--- app/repositories/postgres/helper.py
+++ app/repositories/postgres/helper.py
@@ -5,7 +5,7 @@ def select_query_helper(query: str, lock_level: LockLevel):
     query = query.strip(";")
 
     match lock_level:
-        case LockLevel.EXCLUSIVE:
+        case LockLevel.MODIFY_LOCK:
             query += " FOR UPDATE"
         case LockLevel.NONE:
             """Do nothing"""
--- tests/repositories/postgres/test_helper.py
+++ tests/repositories/postgres/test_helper.py
@@ -2,10 +2,10 @@ from app.repositories.base import LockLevel
 from app.repositories.postgres.helper import select_query_helper
 
 
-def test_should_select_query_helper_add_for_update_when_lock_level_exclusive():
+def test_should_select_query_helper_add_for_update_when_lock_level_modify_lock():
     query = "SELECT * FROM table WHERE id = 2"
     assert (
-        select_query_helper(query, lock_level=LockLevel.EXCLUSIVE)
+        select_query_helper(query, lock_level=LockLevel.MODIFY_LOCK)
         == "SELECT * FROM table WHERE id = 2 FOR UPDATE;"
     )
 
@@ -21,6 +21,6 @@ def test_should_select_query_helper_add_nothing_when_lock_level_none():
 def test_should_select_query_helper_move_semi_colon_to_the_end_even_if_need_to_append_sth_to_query():
     query = "SELECT * FROM table WHERE id = 2;"
     assert (
-        select_query_helper(query, lock_level=LockLevel.EXCLUSIVE)
+        select_query_helper(query, lock_level=LockLevel.MODIFY_LOCK)
         == "SELECT * FROM table WHERE id = 2 FOR UPDATE;"
     )
